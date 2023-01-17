e454a5c84b594bf287b9c98f87cb5fa4e38ed47f
Lihaozhe <w18942020649@163.com>
2023-01-17 11:32:12 +0800

    小更新

--- main.py
+++ main.py
@@ -19,37 +19,46 @@ def EncodeTime(times):
 
 
 def writeIn(info):
-    files = open("all.json", mode="w",encoding="utf-8")  # 以写入模式打开文件
+    files = open("all.json", mode="w", encoding="utf-8")  # 以写入模式打开文件
     files.write(info)
     files.close()
 
 
+def showDeadMessage(message):
+    print(message)
+    time.sleep(5)
+    exit(message)
+
+
 time_cost = []
-file = open("all.json",encoding="utf-8")
-currentData = json.loads(file.read())
-file.close()
+try:
+    file = open("all.json", encoding="utf-8")
+    currentData = json.loads(file.read())
+    file.close()
+except:
+    showDeadMessage("请检查all.json配置是不是有问题")
 print("从文件中读取的数据：")
 for i in range(0, len(currentData)):
     print(currentData[i]['content'], "剩余:",
           currentData[i]['page'], "序号：[", i, "]")
-
-currentTracking = int(input("请输入序号"))
-
-pages = currentData[currentTracking]['page']
+try:
+    currentTracking = int(input("请输入序号"))
+    pages = currentData[currentTracking]['page']
+except:
+    showDeadMessage("输入了非法的数字")
 while True:
     time_start = time.time()
     print("请专注....\n此面写完后请按下此按钮")
+    # 判断pages的值
+    if pages <= 0:
+        showDeadMessage("当前作业已经完成")
+    # 判断完毕
     os.system("pause")
     pages -= 1
     # 将更新的数据写入all.json
     currentData[currentTracking]['page'] = pages
     writeIn(str(json.dumps(currentData, ensure_ascii=False)))
     # 写入完毕
-    # 判断pages的值
-    if pages <= 0:
-        print("当前科目作业已经完成！")
-        exit()
-    # 判断完毕
     time_end = time.time()
     time_cost.append(time_end-time_start)
     os.system("cls")
